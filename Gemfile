86ad3410d7e95573794c00f4a92c36f84aeaf415
Srđan Vidović <vidosrd@gmail.com>
2024-11-15 22:06:09 +0100

    123

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source "https://rubygems.org"
 
-#gem 'rake', '~> 13.2', '>= 13.2.1'
 gem 'rails', '~> 8.0'
 gem "propshaft"
 gem 'pg', '~> 1.5', '>= 1.5.9'
@@ -10,7 +9,7 @@ gem "turbo-rails"
 gem "stimulus-rails"
 gem "tailwindcss-rails"
 gem "jbuilder"
-# gem "bcrypt", "~> 3.1.7"
+gem 'bcrypt', '~> 3.1', '>= 3.1.20'
 gem "tzinfo-data", platforms: %i[ windows jruby ]
 gem "solid_cache"
 gem "solid_queue"
@@ -24,6 +23,14 @@ gem 'madmin', '~> 1.2', '>= 1.2.11'
 #gem 'pagy', '~> 9.2', '>= 9.2.2'
 gem 'pagy', '~> 8.6', '>= 8.6.3'
 gem 'cssbundling-rails', '~> 1.4', '>= 1.4.1'
+gem "appraisal"
+
+gem 'ffaker', '~> 2.23'
+gem 'friendly_id', '~> 5.5', '>= 5.5.1'
+gem 'name_of_person', '~> 1.1', '>= 1.1.3'
+gem 'paper_trail', '~> 16.0'
+gem 'standardrb', '~> 1.0', '>= 1.0.1'
+gem 'web-console', '~> 4.2', '>= 4.2.1', group: :development
 
 group :development, :test do
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
@@ -32,7 +39,7 @@ group :development, :test do
 end
 group :development do
   gem 'solargraph'               # run the Solargraph rebuild command in VSC
-  gem "web-console"
+  #gem "web-console"
 end
 group :test do
   gem 'activesupport'
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,3 @@
-# Pin npm packages by running ./bin/importmap
-
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
@@ -7,3 +5,16 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
+
+pin "application-madmin", to: "madmin/application.js", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.esm.js"
+pin_all_from Madmin::Engine.root.join("app/javascript/madmin/controllers"), under: "controllers", to: "madmin/controllers"
+
+pin "tom-select", to: "https://unpkg.com/tom-select@2/dist/esm/tom-select.complete.js"
+pin "tailwindcss-stimulus-components", to: "https://unpkg.com/tailwindcss-stimulus-components@5/dist/tailwindcss-stimulus-components.module.js"
+pin "flatpickr", to: "https://unpkg.com/flatpickr@4/dist/esm/index.js"
+pin "stimulus-flatpickr", to: "https://unpkg.com/stimulus-flatpickr@3.0.0-0/dist/index.m.js"
